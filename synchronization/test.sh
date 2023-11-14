2c15fb5113c4a5bc521db52794162a3c9c0b0c48
jingyz <jingyz@umich.edu>
2023-11-14 15:49:45 -0500

    Clean some of the files in sync dir

--- synchronization/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=www.nist.gov_1
-url="http://web.archive.org/web/20230422131348/https://www.nist.gov/world-trade-center-investigation"
-
-node log_writes_replay.js -d writes/$dir -f wayback -w $url
