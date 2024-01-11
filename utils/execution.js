bb2aef96299e28036e500835f803e266e4cd9b3d
jingyz <jingyz@umich.edu>
2024-01-11 13:55:08 -0500

    Forget 1 file from last commit...

--- utils/execution.js
+++ utils/execution.js
@@ -16,6 +16,7 @@ function parseStack(stack){
             callFrames.push({
                 functionName: callFrame.functionName,
                 url: callFrame.url,
+                // line and column numbers are 0-based
                 lineNumber: callFrame.lineNumber,
                 columnNumber: callFrame.columnNumber
             })
@@ -69,7 +70,47 @@ class executionStacks {
     }
 }
 
+class ExecutableResources {
+    resources = {};
+    constructor(){
+        this.resources = [];
+        this.filterList = ['image', 'video', 'audio', 'application/pdf']
+    }
+
+    async onResponse(response) {
+        const request = response.request();
+        const statuscode = response.status();
+        if (statuscode >= 300) // Redirection
+            return;
+        // Check mime type of the response, if image or video, skip adding to resources
+        const mime = response.headers()['content-type'];
+        if (mime) {
+            for (const filter of this.filterList) {
+                if (mime.includes(filter)){
+                    console.log("Filtered", request.url());
+                    return;
+                }
+            }
+        }
+        let responseObj = {
+            url: request.url(),
+            status: statuscode,
+            method: request.method(),
+        }
+
+        try {
+            const responseBody = await response.text();
+            responseObj['body'] = responseBody;
+        } catch (err) {
+            // console.log("\tGot exception on response.body", err.message, request.url());
+            return;
+        }
+        this.resources[responseObj.url] = responseObj;
+    }
+}
+
 module.exports = {
     parseStack,
-    executionStacks
+    executionStacks,
+    ExecutableResources
 }
